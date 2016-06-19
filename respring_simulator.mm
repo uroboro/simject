4c9d240b5f425bf94d903aa92275224341d0cf3c
Federico Sandri <uroboro.845@gmail.com>
2016-06-19 18:12:17 -0300

    Change system to NSTask

--- respring_simulator.mm
+++ respring_simulator.mm
@@ -1,11 +1,23 @@
 #import "simjectCore.h"
 
+@interface NSTask : NSObject
++ (NSTask *)launchedTaskWithLaunchPath:(NSString *)path
+							 arguments:(NSArray<NSString *> *)arguments;
+@end
+
 int main(int argc, const char * argv[]) {
 	printf("respring_simulator (C) 2016 Karen Tsai (angelXwind)\n");
 	printf("Injecting appropriate dynamic libraries from /opt/simject...\n");
-	NSString *envVars;
-	system([[NSString stringWithFormat:@"xcrun simctl spawn booted launchctl debug system/com.apple.SpringBoard%@", (envVars = simjectGenerateDylibList(nil)) ? [NSString stringWithFormat:@" --environment DYLD_INSERT_LIBRARIES=%@", envVars] : nil] UTF8String]);
+	NSArray *t_args = @[ @"simctl", @"spawn", @"booted", @"launchctl", @"debug", @"system/com.apple.SpringBoard"];
+	NSString *envVars = simjectGenerateDylibList(nil);
+	if (envVars) {
+		NSString *dyld = [NSString stringWithFormat:@" --environment DYLD_INSERT_LIBRARIES=%@", envVars];
+		t_args = [t_args arrayByAddingObject:dyld];
+	}
+	[NSTask launchedTaskWithLaunchPath:@"/usr/bin/xcrun" arguments:t_args];
+	
 	printf("Respringing...\n");
-	system("xcrun simctl spawn booted launchctl stop com.apple.SpringBoard");
+	t_args = @[ @"simctl", @"spawn", @"booted", @"launchctl", @"stop", @"system/com.apple.SpringBoard"];
+	[NSTask launchedTaskWithLaunchPath:@"/usr/bin/xcrun" arguments:t_args];
 	return 0;
 }
